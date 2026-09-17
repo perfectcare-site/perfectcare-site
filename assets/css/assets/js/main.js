2db4510d1be9d6e9587170526386048f8836570e
perfectcare-site <perfectcare72139@gmail.com>
2026-09-18 06:10:49 +0900

    Delete assets/css directory

--- assets/css/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-
-const btn=document.querySelector('.menu-toggle');
-const menu=document.querySelector('.menu');
-if(btn && menu){
-  btn.addEventListener('click',()=>{
-    const open=menu.classList.toggle('open');
-    btn.setAttribute('aria-expanded',open?'true':'false');
-  });
-}
